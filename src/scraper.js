6c597f574c8a00df50696e6f13ea9cecd55d5a3d
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-09-01 14:54:20 +0200

    add allrecipes scraper

--- src/scraper.js
+++ src/scraper.js
@@ -14,26 +14,21 @@ async function scrapeWeb(site) {
 }
 
 async function giallozafferano(site, originalSite) {
+  // {
+  //   "uri": "https://www.giallozafferano.it/ricette-cat/",
+  //   "totalPages": 437,
+  //   "downloadedPages": 5
+  // },
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
   await page.goto(site, { waitUntil: "domcontentloaded" });
   const articles = await page.$$("article.gz-card");
+
   let result = await Promise.all(
     articles.map(async (el) => {
       return await el.evaluate((x) => x.innerHTML.replace("\t", ""));
     })
   );
-  // const $ = cheerio.load(result[0]);
-  // //nome ricetta
-  // console.log($(".gz-title").text());
-  // //link ricetta
-  // console.log($(".gz-title a").attr("href"));
-  // //immagine ricetta
-  // console.log($(".gz-card-image img").attr("src"));
-  // //descrizione ricetta
-  // console.log($(".gz-description").text());
-  // //categoria
-  // console.log($(".gz-category").text());
 
   for (let i of result) {
     const $ = cheerio.load(i);
@@ -65,7 +60,66 @@ async function giallozafferano(site, originalSite) {
   await browser.close();
 }
 
-async function allrecipes(site) {}
+async function allrecipes(site) {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto(site, { waitUntil: "domcontentloaded" });
+
+  const articles = await page.$$(".mntl-card-list");
+  let result = await Promise.all(
+    articles.map(async (el) => {
+      return await el.evaluate((x) => x.innerHTML.replace("\t", ""));
+    })
+  );
+
+  const $ = cheerio.load(result[0]);
+  // console.log(result[0]);
+  // url
+  // console.log($("a").attr("href"));
+  // //category
+  // console.log($(".card__content").data("tag"));
+  // //desc
+
+  // //img
+  // console.log($("img").attr("src"));
+  //lang
+
+  //name
+  // console.log($(".card__title-text"));
+  const url = $("a").attr("href");
+
+  await browser.close();
+  await newSearch(url);
+
+  // for (let i of result) {
+  //   const $ = cheerio.load(i);
+  //   await recipeModel.create({
+  //     url: $("a").attr("href"),
+  //     category: $(".card__content").data("tag"),
+  //     desc: ,
+  //     img: $("img").attr("src"),
+  //     lang: ,
+  //     name: ,
+  //   });
+  // }
+}
+
+async function newSearch(url) {
+  const browser2 = await puppeteer.launch({ headless: "new" });
+  const page2 = await browser2.newPage();
+  await page2.goto(url, { waitUntil: "domcontentloaded" });
+  const article = await page2.$$(".article-post-header");
+  let result = await Promise.all(
+    article.map(async (el) => {
+      return await el.evaluate((x) => x.innerHTML.replace("\t", ""));
+    })
+  );
+  const $ = cheerio.load(result[0]);
+  //nome
+  console.log($("#article-heading_1-0").text());
+  //desc
+  console.log($("#article-subheading_1-0").text());
+}
 
 module.exports = {
   scrapeWeb,
