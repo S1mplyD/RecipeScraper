1ed1286990f31cc9d97f352403d5f733b55deff7
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-09-06 16:57:02 +0200

    fix recipe model

--- database/recipe.model.js
+++ database/recipe.model.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
   url: { type: String, required: true, unique: true },
-  img: { type: String, required: true },
+  img: { type: String },
   name: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { type: String },
   desc: { type: String, required: true },
   lang: { type: String, required: true },
+  featuredData: { type: [String] },
 });
 
 const recipeModel = mongoose.model("recipeData", recipeSchema);
