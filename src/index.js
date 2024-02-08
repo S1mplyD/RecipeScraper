a03ac787e564073124e67761e249a9c895f542f5
S1mplyD <lucabennati2k16@gmail.com>
2024-02-08 17:36:00 +0000

    refactor: remove file structure comment

--- src/index.js
+++ src/index.js
@@ -9,72 +9,21 @@ const fixCategories = require("./fixCategories").fixCategories;
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.MONGO || "")
-  .then(async () => {
-    // let sitesRaw = fs.readFileSync("sites.json");
-    // const sites = JSON.parse(sitesRaw);
-    //
-    console.log("connected to mongoose");
-    // console.log("[Preparing to steal some data...]");
-    // // await deleteRegex("allrecipes");
-    // for (i of sites) {
-    //   await scrapeWeb(i);
-    // }
-    const update = await fixCategories();
-    console.log(update);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    .connect(process.env.MONGO || "")
+    .then(async () => {
+        // let sitesRaw = fs.readFileSync("sites.json");
+        // const sites = JSON.parse(sitesRaw);
+        //
+        console.log("connected to mongoose");
+        // console.log("[Preparing to steal some data...]");
+        // // await deleteRegex("allrecipes");
+        // for (i of sites) {
+        //   await scrapeWeb(i);
+        // }
+        const update = await fixCategories();
+        console.log(update);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
-// ,
-// {
-//   "uri": "https://www.allrecipes.com/recipes/17138/world-cuisine/european/french/main-dishes/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/1828/world-cuisine/european/french/desserts/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/1829/world-cuisine/european/french/soups-and-stews/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/1848/world-cuisine/european/french/appetizers/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/16126/world-cuisine/european/french/french-bread/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/17846/world-cuisine/european/spanish/main-dishes/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/17847/world-cuisine/european/spanish/appetizers/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/17848/world-cuisine/european/spanish/soups-and-stews/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/242/drinks/sangria/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// },
-// {
-//   "uri": "https://www.allrecipes.com/recipes/716/world-cuisine/european/eastern-european/russian/",
-//   "totalPages": 1,
-//   "downloadedPages": 0
-// }
