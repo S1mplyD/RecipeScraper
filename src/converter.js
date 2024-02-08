192f16206fc4b2d52ab2cf163ace26bfc9cd9a71
S1mplyD <lucabennati2k16@gmail.com>
2024-02-08 17:36:29 +0000

    feat: add text converter for recipes

---
+++ src/converter.js
@@ -0,0 +1,9 @@
+const { recipeModel }= require("../database/recipe.model")
+
+const convertRecipeToText = async ()=>{
+    const recipes = await recipeModel.find()
+}
+
+const convertGZ = async ()=>{}
+
+const convertAllRecipe = async ()=>{}
