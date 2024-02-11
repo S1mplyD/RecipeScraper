335cee0122d4047a3abecfeb0afabe6959536a6e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-02-11 15:26:14 +0100

    feat: update converter

--- src/converter.js
+++ src/converter.js
@@ -1,9 +1,26 @@
-const { recipeModel }= require("../database/recipe.model")
+const recipeModel = require("../database/recipe.model");
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
-const convertRecipeToText = async ()=>{
-    const recipes = await recipeModel.find()
-}
+const convertRecipeToText = async () => {
+  const recipes = await recipeModel.find();
+  for (let i of recipes) {
+    if (i.url.includes("giallozafferano")) {
+      await convertGZ(i);
+    } else await convertAllRecipe(i);
+  }
+};
 
-const convertGZ = async ()=>{}
+const convertGZ = async (recipe) => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto(recipe.url, { waitUntil: "domcontentloaded" });
+};
 
-const convertAllRecipe = async ()=>{}
+const convertAllRecipe = async (recipe) => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto(recipe.url, { waitUntil: "domcontentloaded" });
+};
+
+module.exports = { convertRecipeToText };
