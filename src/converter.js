c8ba61d45d8806ac46c6452c695d567cc6e354f1
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2024-02-13 19:28:01 +0100

    add recipe converter for allrecipes

--- src/converter.js
+++ src/converter.js
@@ -3,7 +3,9 @@ const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
 const convertRecipeToText = async () => {
-  const recipes = await recipeModel.find();
+  const recipes = await recipeModel.find({
+    url: { $regex: "allrecipes", $options: "i" },
+  });
   for (let i of recipes) {
     if (i.url.includes("giallozafferano")) {
       await convertGZ(i);
@@ -18,9 +20,50 @@ const convertGZ = async (recipe) => {
 };
 
 const convertAllRecipe = async (recipe) => {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-  await page.goto(recipe.url, { waitUntil: "domcontentloaded" });
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+    await page.goto(recipe.url, { waitUntil: "domcontentloaded" });
+    const ingredients = await page.$$(
+      ".mntl-structured-ingredients__list-item",
+    );
+
+    let result = await Promise.all(
+      ingredients.map(async (el) => {
+        return await el.evaluate((x) => x.innerHTML.replace("\t", ""));
+      }),
+    );
+    console.log(recipe.url);
+
+    let ingredientsString = [];
+    for (let i in result) {
+      const $ = cheerio.load(result[i]);
+      let inString = $("p").text();
+      ingredientsString.push(inString);
+    }
+    const directions = await page.$("#recipe__steps_1-0");
+    const newresult = await directions.evaluate((x) =>
+      x.innerHTML.replace("\t", ""),
+    );
+    const $ = cheerio.load(newresult);
+    $("figure").remove();
+    let directionsString = $("li")
+      .text()
+      .replaceAll("\t", "")
+      .replaceAll("\n", "");
+
+    await browser.close();
+
+    await recipeModel.updateOne(
+      { url: recipe.url },
+      {
+        $set: { ingredients: ingredientsString },
+        directions: directionsString,
+      },
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = { convertRecipeToText };
--- src/index.js
+++ src/index.js
@@ -5,25 +5,26 @@ const { resolve } = require("path");
 config({ path: resolve(__dirname, "..", ".env") });
 const fs = require("fs");
 const { deleteRegex } = require("./delete");
+const { convertRecipeToText } = require("./converter");
 const fixCategories = require("./fixCategories").fixCategories;
 
 mongoose.set("strictQuery", false);
 mongoose
-    .connect(process.env.MONGO || "")
-    .then(async () => {
-        // let sitesRaw = fs.readFileSync("sites.json");
-        // const sites = JSON.parse(sitesRaw);
-        //
-        console.log("connected to mongoose");
-        // console.log("[Preparing to steal some data...]");
-        // // await deleteRegex("allrecipes");
-        // for (i of sites) {
-        //   await scrapeWeb(i);
-        // }
-        const update = await fixCategories();
-        console.log(update);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
+  .connect(process.env.MONGO || "")
+  .then(async () => {
+    // let sitesRaw = fs.readFileSync("sites.json");
+    // const sites = JSON.parse(sitesRaw);
+    //
+    console.log("connected to mongoose");
+    // console.log("[Preparing to steal some data...]");
+    // // await deleteRegex("allrecipes");
+    // for (i of sites) {
+    //   await scrapeWeb(i);
+    // }
+    // const update = await fixCategories();
+    // console.log(update);
+    await convertRecipeToText();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
